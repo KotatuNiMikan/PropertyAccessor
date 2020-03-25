b547183e2a2b8d9fe36a191f611e096735cb0d44
kotatu <ms.18e.kampfer.bset@gmail.com>
2020-03-26 01:21:01 +0900

    最初のコミット

---
+++ PropertyAccessor.Test/PropertyAccessorTest.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PropertyAccessorTest.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace PropertyAccessor
+{
+    /// <summary>
+    /// <see cref="PropertyAccessor"/>のテストクラスです。
+    /// </summary>
+    [TestClass]
+    public class PropertyAccessorTest
+    {
+        /// <summary>
+        /// <see cref="PropertyAccessor.GetLambda{TSource, TResult}"/>のテストメソッドです。
+        /// </summary>
+        /// <param name="index">列挙のインデックスです。</param>
+        /// <param name="expected">期待値です。</param>
+        [DataTestMethod]
+        [DataRow(0, "A")]
+        [DataRow(1, "B-C")]
+        [DataRow(2, "B-D")]
+        public void Test(int index, string expected)
+        {
+            var hoge = new Hoge
+            {
+                A = "A",
+                B = new Foo
+                {
+                    C = "B-C",
+                    D = "B-D",
+                }
+            };
+
+            var actual = PropertyAccessor.GetLambda<Hoge, string>().ElementAt(index);
+            var func = actual.Compile();
+            Assert.AreEqual(expected, func(hoge), actual.ToString());
+        }
+
+        /// <summary>
+        /// ダミークラスです。
+        /// </summary>
+        private class Hoge
+        {
+            /// <summary>
+            /// 検出対象のプロパティです。
+            /// </summary>
+            public string A { get; set; }
+
+            /// <summary>
+            /// このプロパティの中身も検出対象です。
+            /// </summary>
+            public Foo B { get; set; }
+        }
+
+        /// <summary>
+        /// ダミークラスです。
+        /// </summary>
+        private class Foo
+        {
+            /// <summary>
+            /// 検出対象のプロパティです。
+            /// </summary>
+            public string C { get; set; }
+
+            /// <summary>
+            /// 検出対象のプロパティです。
+            /// </summary>
+            public string D { get; set; }
+
+            /// <summary>
+            /// 検出対象外のプロパティです。
+            /// </summary>
+            public bool E { get; set; }
+        }
+    }
+}
---
+++ PropertyAccessor/PropertyAccessor.cs
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="PropertyAccessor.cs" company="CompanyName">
+//     Company copyright tag.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace PropertyAccessor
+{
+    /// <summary>
+    /// プロパティにアクセスするラムダ式を作成します。
+    /// </summary>
+    public static class PropertyAccessor
+    {
+        /// <summary>
+        /// プロパティにアクセスするラムダ式を作成します。
+        /// </summary>
+        /// <typeparam name="TSource">ラムダ式のパラメータとなる型です。</typeparam>
+        /// <typeparam name="TResult">アクセス対象のプロパティの型です。</typeparam>
+        /// <returns>ラムダの列挙です。対象がない場合、空の列挙を返します。</returns>
+        public static IEnumerable<Expression<Func<TSource, TResult>>> GetLambda<TSource, TResult>()
+        {
+            var arg = Expression.Parameter(typeof(TSource), "x");
+            return GetLabda<TSource, TResult>(arg, typeof(TSource).GetProperties(), arg);
+        }
+
+        /// <summary>
+        /// プロパティにアクセスするラムダ式を作成します。
+        /// </summary>
+        /// <typeparam name="TSource">ラムダ式のパラメータとなる型です。</typeparam>
+        /// <typeparam name="TResult">アクセス対象のプロパティの型です。</typeparam>
+        /// <param name="arg">パラメータ用の<see cref="ParameterExpression"/>です。</param>
+        /// <param name="propertyInfo">プロパティです。</param>
+        /// <param name="member">プロパティへの<see cref="Expression"/>です。</param>
+        /// <returns>ラムダの列挙です。対象がない場合、空の列挙を返します。</returns>
+        private static IEnumerable<Expression<Func<TSource, TResult>>> GetLabda<TSource, TResult>(
+            ParameterExpression arg,
+            PropertyInfo[] propertyInfo,
+            Expression member)
+        {
+            var result = new List<Expression<Func<TSource, TResult>>>();
+
+            propertyInfo.ToList()
+                .ForEach(prop =>
+                {
+                    var newMember = Expression.PropertyOrField(member, prop.Name);
+                    if (prop.PropertyType == typeof(TResult))
+                    {
+                        result.Add(Expression.Lambda<Func<TSource, TResult>>(newMember, arg));
+                        return;
+                    }
+
+                    result.AddRange(GetLabda<TSource, TResult>(arg, prop.PropertyType.GetProperties(), newMember));
+                });
+            return result;
+        }
+    }
+}
